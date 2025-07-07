62bff8ea446494156c778ede02b8ba62d78ca0f2
Raulito2504 <raulariasrafael@gmail.com>
2025-07-07 12:20:01 -0500

    prt_09

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,16 +2,16 @@ import axios from "axios";
 
 export const api = axios.create({
     baseURL: "http://localhost:4000/api",
-    withCredentials: true, // Si necesitas enviar cookies
+    withCredentials: true, 
 });
 
-// Ejemplo de request GET
+
 export const getPing = async () => {
     const res = await api.get("/");
     return res.data;
 };
 
-// Ejemplo de request POST (login)
+
 export const login = async (nombre_usuario: string, password: string) => {
     const res = await api.post("/auth/login", { nombre_usuario, password });
     return res.data;
